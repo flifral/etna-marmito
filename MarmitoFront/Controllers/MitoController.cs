bf71fd10b931aa8c49cb30ae34267e9e156d8e90
flifral <15982596+flifral@users.noreply.github.com>
2018-05-15 18:19:41 +0200

    filter by category

--- MarmitoFront/Controllers/MitoController.cs
+++ MarmitoFront/Controllers/MitoController.cs
@@ -16,7 +16,7 @@ namespace MarmitoFront.Controllers
     {
         private API m_api = new API();
         [HttpGet]
-        public async Task<IActionResult> Index(string searchString, int? page)
+        public async Task<IActionResult> Index(string searchString, string indisp, string famille, string travail, string amour, int? page)
         {
             if (!HttpContext.Request.Cookies.ContainsKey("tokenValue"))
             {
@@ -37,6 +37,26 @@ namespace MarmitoFront.Controllers
             if (!String.IsNullOrEmpty(searchString))
             {
                 mitos = mitos.Where(m => m.User.Name.Contains(searchString)).ToList();
+            }
+
+            if (!String.IsNullOrEmpty(indisp) && indisp == "false")
+            {
+                mitos = mitos.Where(m => m.Mito.Category != MarmitoAPI.Models.Mito.CategoryList.INDISP).ToList();
+            }
+
+            if (!String.IsNullOrEmpty(famille) && famille == "false")
+            {
+                mitos = mitos.Where(m => m.Mito.Category != MarmitoAPI.Models.Mito.CategoryList.FAMILY).ToList();
+            }
+
+            if (!String.IsNullOrEmpty(travail) && travail == "false")
+            {
+                mitos = mitos.Where(m => m.Mito.Category != MarmitoAPI.Models.Mito.CategoryList.WORK).ToList();
+            }
+
+            if (!String.IsNullOrEmpty(amour) && amour == "false")
+            {
+                mitos = mitos.Where(m => m.Mito.Category != MarmitoAPI.Models.Mito.CategoryList.LOVE).ToList();
             }
 
             return View(PaginatedList<MarmitoAPI.Models.MitoUser>.Create(mitos, page ?? 1, 3));
